9629452bbe4bc3c79bb58a46182577948a461f8d
Vladislav Zhokhov <v.zhokhov@tinkoff.ru>
2021-09-09 22:58:12 +0300

    Create Detail Moview Info View

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by v.zhokhov on 09.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
